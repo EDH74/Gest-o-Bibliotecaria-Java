8c4bd1dbca6e7d5bc28655c774c017cbb50bbcaf
Rafael Borges <rafaelbs3377@gmail.com>
2026-05-20 14:16:09 -0300

    Adição das variáveis e dos Getters e Setters da classe Obra.

--- src/classes/Obra.java
+++ src/classes/Obra.java
@@ -1,14 +1,54 @@
 package classes;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author rbzin
- */
 public class Obra {
+    String titulo, isbn, data, editora,autores;
+
+    //getter e setter de Titulo!
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    //getter e setter de isbn!
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    //getter e setter de data!
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    //getter e setter de editora!
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    //getter e setter de autores!
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+    
+    
+    
     
 }
