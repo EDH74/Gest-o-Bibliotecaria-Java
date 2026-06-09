f1856ce3ca51369499774d0c50dd62180a5912c3
Eduardo MDM <eduardoprimo2017@gmail.com>
2026-06-08 22:30:04 -0300

    Ajeitando umas parada ae

--- src/classes/FichaConservacao.java
+++ src/classes/FichaConservacao.java
@@ -107,11 +107,7 @@ public class FichaConservacao implements GerenciarFichaConservacao{
         return null;
     }
     
-    public void excluirPeloIndice(Intervencao inter){
-        int indice = listaIntervencoes.indexOf(inter);
-        listaIntervencoes.remove(indice);
-        atualizarIdFichas();
-    }
+  
     
    
 }
